1ef0ff0f995f7edfd063761882e219df46d93392
LJBox <contact@ljbox.fr>
2026-03-04 20:12:48 +0100

    made clean function and trying to get stream working

--- main.c
+++ main.c
@@ -17,90 +17,107 @@
 uint8_t frame_buffer[FRAME_SIZE];
 int frame_ready = 0;
 
-
-int init()
-{
-    int error = 0;
-    int claim_0;
-    int claim_1;
-    libusb_context *ctx = NULL;
-    libusb_device_handle *dev;
-
-    int result = libusb_init(&ctx);
-    if (result != 0) {
-        error = 1;
-        goto cleanup;
-    }
-    //printf("%d\n", result);
-    
-    dev = libusb_open_device_with_vid_pid(ctx, VID, PID);
-    if (dev == NULL) {
-        error = 1;
-        goto cleanup;
-    };
-
-    claim_0 = libusb_claim_interface(dev, 0);
-    if (claim_0 != 0){
-        error = 1;
-        goto cleanup;
-    };
-
-    int init_crtl = esp770u_init_unknown(dev);
-    printf("%d\n", init_crtl);
-    if (init_crtl < 0) {
-        error = 1;
-        goto cleanup;
-    };
-
-    int init_sensor = ar0134_init(dev);
-    printf("%d\n", init_sensor);
-    if (init_sensor < 0) {
-        error = 1;
-        goto cleanup;
-    };
-
-    int set_frame_timings = ar0134_set_timings(dev, true);
-    printf("%d\n", set_frame_timings);
-    if (set_frame_timings < 0) {
-        error = 1;
-        goto cleanup;
-    };
-
-    int set_ae_control = ar0134_set_ae(dev, true); // can be changed for manual, and other param
-    printf("%d\n", set_ae_control);
-    if (set_ae_control < 0) {
-        error = 1;
-        goto cleanup;
-    };
-
-    int set_sync = ar0134_set_sync(dev, true);
-    printf("%d\n", set_sync);
-    if (set_sync < 0) {
-        error = 1;
-        goto cleanup;
-    };
-
-    claim_1 = libusb_claim_interface(dev, 1);
-    if (claim_1 != 0){
-        error = 1;
-        goto cleanup;
-    };
-    
-cleanup:
+void clean(libusb_context *ctx, libusb_device_handle *dev, int interface_0, int interface_1) {
     if (dev != NULL) {
-        if (claim_0 == 0) libusb_release_interface(dev, 0);
-        if (claim_1 == 0) libusb_release_interface(dev, 1);
+        if (interface_0 == 0) libusb_release_interface(dev, interface_0);
+        if (interface_1 == 0) libusb_release_interface(dev, interface_1);
         libusb_close(dev);
     }
     if (ctx != NULL) {
         libusb_exit(ctx);
     }
-    
-    return error;
 }
 
-int strem(){
+int init()
+{
+    int claim_0;
+    int claim_1;
+    struct uvc_probe_commit_control probe = {0};
+    probe.bmHint = 0x0001;
+    probe.bFormatIndex = 1;
+    probe.bFrameIndex = 4;
+    probe.dwFrameInterval= 0x00020EE0;
+    probe.dwMaxVideoFrameSize = FRAME_SIZE;
+    probe.dwMaxPayloadTransferSize = 0x0C00;
+    libusb_context *ctx = NULL;
+    libusb_device_handle *dev;
+
+    int result = libusb_init(&ctx);
+    if (result != 0) {
+        clean(ctx, dev, claim_0, claim_1);
+        return 1;
+    }
+    //printf("%d\n", result);
     
+    dev = libusb_open_device_with_vid_pid(ctx, VID, PID);
+    if (dev == NULL) {
+        clean(ctx, dev, claim_0, claim_1);
+        return 1;
+    };
+
+    claim_0 = libusb_claim_interface(dev, 0);
+    if (claim_0 != 0){
+        clean(ctx, dev, claim_0, claim_1);
+        return 1;
+    };
+
+    int init_crtl = esp770u_init_unknown(dev);
+    printf("%d\n", init_crtl);
+    if (init_crtl < 0) {
+        clean(ctx, dev, claim_0, claim_1);
+        return 1;
+    };
+
+    int init_sensor = ar0134_init(dev);
+    printf("%d\n", init_sensor);
+    if (init_sensor < 0) {
+        clean(ctx, dev, claim_0, claim_1);
+        return 1;
+    };
+
+    int set_frame_timings = ar0134_set_timings(dev, true);
+    printf("%d\n", set_frame_timings);
+    if (set_frame_timings < 0) {
+        clean(ctx, dev, claim_0, claim_1);
+        return 1;
+    };
+
+    int set_ae_control = ar0134_set_ae(dev, true); // can be changed for manual, and other param
+    printf("%d\n", set_ae_control);
+    if (set_ae_control < 0) {
+        clean(ctx, dev, claim_0, claim_1);
+        return 1;
+    };
+
+    int set_sync = ar0134_set_sync(dev, true);
+    printf("%d\n", set_sync);
+    if (set_sync < 0) {
+        clean(ctx, dev, claim_0, claim_1);
+        return 1;
+    };
+
+    claim_1 = libusb_claim_interface(dev, 1);
+    printf("%d\n", claim_1);
+    if (claim_1 != 0){
+        clean(ctx, dev, claim_0, claim_1);
+        return 1;
+    };
+
+    int write = uvc_set_cur(dev, 1, 0, VS_PROBE_CONTROL, &probe, sizeof(probe));
+    printf("%d\n", write);
+    int read = uvc_get_cur(dev, 1, 0, VS_PROBE_CONTROL, &probe, sizeof(probe));
+    printf("%d\n", read);
+    int set = uvc_set_cur(dev, 1, 0, VS_COMMIT_CONTROL, &probe, sizeof(probe));
+    printf("%d\n", set);
+
+    int start_steaming = libusb_set_interface_alt_setting(dev, 1, 2);
+    printf("%d\n", start_steaming);
+    
+    return 0;
+}
+
+int stream(){
+
 }
 
 int main(){
