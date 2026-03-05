5c88ac79217dc059cd26b4b432c9479397697552
LJBox <contact@ljbox.fr>
2026-03-05 16:55:05 +0100

    exit works !

--- main.c
+++ main.c
@@ -17,7 +17,10 @@
 uint8_t frame_buffer[FRAME_SIZE];
 int frame_ready = 0;
 
-void clean(libusb_context *ctx, libusb_device_handle *dev, int interface_0, int interface_1) {
+void clean(libusb_context *ctx, libusb_device_handle *dev, struct libusb_transfer *transfer, int interface_0, int interface_1) {
+    if (transfer) {
+        libusb_cancel_transfer(transfer);
+    }
     if (dev != NULL) {
         if (interface_0 == 0) libusb_release_interface(dev, interface_0);
         if (interface_1 == 0) libusb_release_interface(dev, interface_1);
@@ -41,69 +44,71 @@ int init()
     probe.dwMaxPayloadTransferSize = 0x0C00;
     libusb_context *ctx = NULL;
     libusb_device_handle *dev;
-
+    struct libusb_transfer *transfer = libusb_alloc_transfer(24);
+    uint8_t buffer[16384*24];
+    
     int result = libusb_init(&ctx);
     printf("%d\n", result);
     if (result != 0) {
-        clean(ctx, dev, claim_0, claim_1);
+        clean(ctx, dev, transfer, claim_0, claim_1);
         return 1;
     }
     
     dev = libusb_open_device_with_vid_pid(ctx, VID, PID);
     if (dev == NULL) {
-        clean(ctx, dev, claim_0, claim_1);
+        clean(ctx, dev, transfer, claim_0, claim_1);
         return 1;
-    };
+    }
 
     claim_0 = libusb_claim_interface(dev, 0);
     if (claim_0 != 0){
-        clean(ctx, dev, claim_0, claim_1);
+        clean(ctx, dev, transfer, claim_0, claim_1);
         return 1;
-    };
+    }
 
     int init_crtl = esp770u_init_unknown(dev);
     printf("%d\n", init_crtl);
     if (init_crtl < 0) {
-        clean(ctx, dev, claim_0, claim_1);
+        clean(ctx, dev, transfer, claim_0, claim_1);
         return 1;
-    };
+    }
 
     esp770u_init_radio(dev);
 
     int init_sensor = ar0134_init(dev);
     printf("%d\n", init_sensor);
     if (init_sensor < 0) {
-        clean(ctx, dev, claim_0, claim_1);
+        clean(ctx, dev, transfer, claim_0, claim_1);
         return 1;
-    };
+    }
 
     int set_frame_timings = ar0134_set_timings(dev, true);
     printf("%d\n", set_frame_timings);
     if (set_frame_timings < 0) {
-        clean(ctx, dev, claim_0, claim_1);
+        clean(ctx, dev, transfer, claim_0, claim_1);
         return 1;
-    };
+    }
 
     int set_ae_control = ar0134_set_ae(dev, true); // can be changed for manual, and other param
     printf("%d\n", set_ae_control);
     if (set_ae_control < 0) {
-        clean(ctx, dev, claim_0, claim_1);
+        clean(ctx, dev, transfer, claim_0, claim_1);
         return 1;
-    };
+    }
 
     int set_sync = ar0134_set_sync(dev, false);
     printf("%d\n", set_sync);
     if (set_sync < 0) {
-        clean(ctx, dev, claim_0, claim_1);
+        clean(ctx, dev, transfer, claim_0, claim_1);
         return 1;
-    };
+    }
 
     claim_1 = libusb_claim_interface(dev, 1);
     printf("%d\n", claim_1);
     if (claim_1 != 0){
-        clean(ctx, dev, claim_0, claim_1);
+        clean(ctx, dev, transfer, claim_0, claim_1);
         return 1;
-    };
+    }
 
     int write = uvc_set_cur(dev, 1, 0, VS_PROBE_CONTROL, &probe, sizeof(probe));
     printf("%d\n", write);
@@ -115,24 +120,22 @@ int init()
     int start_steaming = libusb_set_interface_alt_setting(dev, 1, 2);
     printf("%d\n", start_steaming);
     
-    struct libusb_transfer *transfer = libusb_alloc_transfer(24);
-    uint8_t buffer[16384*24];
 
     libusb_fill_iso_transfer(transfer, dev, 0x81, buffer, sizeof(buffer), 24, NULL, NULL, 5000);
 
     libusb_set_iso_packet_lengths(transfer, 16384);
     libusb_submit_transfer(transfer);
-    libusb_handle_events_timeout_completed(ctx, &(struct timeval){.tv_sec = 0, .tv_usec = 100000}, NULL);
-    libusb_handle_events_timeout_completed(ctx, &(struct timeval){.tv_sec = 0, .tv_usec = 100000}, NULL);
-    /* 
+//    libusb_handle_events_timeout_completed(ctx, &(struct timeval){.tv_sec = 0, .tv_usec = 100000}, NULL);
+//    libusb_handle_events_timeout_completed(ctx, &(struct timeval){.tv_sec = 0, .tv_usec = 100000}, NULL);
+
     for (int i = 0; i < 50; i++) {  // 5 seconds
         libusb_handle_events_timeout_completed(ctx, &(struct timeval){.tv_sec = 0, .tv_usec = 100000}, NULL);
-    }*/
+    }
 
     libusb_cancel_transfer(transfer);
     libusb_free_transfer(transfer);
     
-    clean(ctx, dev, claim_0, claim_1);
+    clean(ctx, dev, transfer, claim_0, claim_1);
 
     return 0;
 }
